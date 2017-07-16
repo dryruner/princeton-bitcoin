62b7eacf2f447ff2d62456c712c95b09108a80b5
jonnyhsy <jonnyhsy@foxmail.com>
2017-07-15 17:51:45 -0700

    Update TxHandler and comments.

---
+++ README.md
@@ -0,0 +1,2 @@
+Programming assigments for Princeton University's [Bitcoin and Cryptocurrency Technologies](https://www.coursera.org/learn/cryptocurrency/home/welcome)
+
---
+++ assignment1starterCode/MaxFeeTxHandler.java
@@ -0,0 +1,10 @@
+public class MaxFeeTxHandler extends TxHandler {
+  public MaxFeeTxHandler(UTXOPool utxoPool) {
+    super(utxoPool);
+  }
+
+  // TODO
+  public Transaction[] handleTxs(Transaction[] possibleTxs) {
+    return super.handleTxs(possibleTxs);
+  }
+}
